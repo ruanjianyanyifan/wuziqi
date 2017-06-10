58edd9f1151e0daaa2f01daea415daa8aac15419
ruanjianyanyifan <956266717@qq.com>
2017-06-10 12:50:37 +0800

    Initial commit

---
+++ app/src/main/java/com/example/administrator/wuziqi/wuziqi.java
@@ -0,0 +1,13 @@
+package com.example.administrator.wuziqi;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+
+public class wuziqi extends View{
+    public wuziqi(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+    }
+}
