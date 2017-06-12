b0213fefced434a7c3e3cc41c312df187b0bccf0
ruanjianyanyifan <956266717@qq.com>
2017-06-12 11:35:17 +0800

    test

--- app/src/main/java/com/example/administrator/wuziqi/MainActivity.java
+++ app/src/main/java/com/example/administrator/wuziqi/MainActivity.java
@@ -2,12 +2,19 @@ package com.example.administrator.wuziqi;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
+
+
 }
